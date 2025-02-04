d2a0ac7c6ad0bebea2c853b84f9dda8cf97f62ca
singh-saloni-875 <165870664+singh-saloni-875@users.noreply.github.com>
2025-02-04 16:12:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// let a = 85;
+// let b = 54;
+// console.log(`your pay ${a+b} rupees`);
+// let age = 16;
+// console.log(age<18);
+
+// let msg = "    hello    ";
+// console.log(msg);
+// let newmsg= msg.trim().toUpperCase();
+// console.log(newmsg)
+// let msg = "I Love you! , Saloni\n";
+
+// // console.log(msg.slice(-2));
+// console.log(msg.repeat(6))
+let fruit = ["mango","banana", "apple" ];
+fruit[0] = "saloni";
+console.log(fruit);
